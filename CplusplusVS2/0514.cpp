9ae957eacc1e3e46dd0105ec03da5b3eee95b0f2
jimin <jjimini98@naver.com>
2021-05-27 20:26:31 +0900

    proceeding 0514

--- CplusplusVS2/0514.cpp
+++ CplusplusVS2/0514.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-using namespace std;
-
-class Circle {
-	int radius;
-public:
-	Circle(int radius = 1) { this->radius = radius; }
-
-
-
-
-};
+//#include <iostream>
+//using namespace std;
+//
+//class Circle {
+//	int radius;
+//public:
+//	Circle(int radius = 1) { this->radius = radius; }
+//
+//
+//
+//
+//};
